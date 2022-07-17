ff431f034b2d3f5a3ba7b812c81a2572488ee8b8
SokyShop <108899509+SokyFre2@users.noreply.github.com>
2022-07-17 08:58:16 -0400

    Update session_generator.py

--- session_generator.py
+++ session_generator.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 
-api_id = input("Enter API_ID:\n")
-api_hash = input("Enter API_HASH:\n")
-bot_token = input("Enter BOT_TOKEN:\n")
+api_id = input("16376800\n")
+api_hash = input("506da3fde32cd6487709c5e2fd67cd43\n")
+bot_token = input("5502789457:AAGTeio92053LZfVD_gquNQpL5zl_JiedpM\n")
 
 client = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 client.start()
